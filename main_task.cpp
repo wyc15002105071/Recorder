89025456c0c951a923c5b426cf4f599e4e43cb22
wyc15002105071 <747550962@qq.com>
2023-11-25 02:23:29 -0800

    [MainWidget]:finish main widget ui

---
+++ main_task.cpp
@@ -0,0 +1,6 @@
+#include "main_task.h"
+
+MainTask::MainTask()
+{
+
+}
---
+++ main_task.h
@@ -0,0 +1,11 @@
+#ifndef MAINTASK_H
+#define MAINTASK_H
+
+
+class MainTask
+{
+public:
+    MainTask();
+};
+
+#endif // MAINTASK_H
