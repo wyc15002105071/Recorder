de8217f4febd016ba2b95f3cc43a2f2c7373f3b1
wyc15002105071 <74755962@qq.com>
2023-12-13 23:08:56 -0800

    update third-party of linux_arm32

--- make.sh
+++ make.sh
@@ -1,5 +1,4 @@
-#SDK_DIR=/home/wangyichen/linux-sdk/rk3588_linux_sdk
-SDK_DIR=/home2/shinengqiang/RK3588_EVB
+SDK_DIR=/home/wangyichen/linux-sdk/rk3588_linux_sdk
 export QMAKE_SYSROOT=${SDK_DIR}/buildroot/output/rockchip_rk3588/host/aarch64-buildroot-linux-gnu/sysroot
 
 QMAKE=${SDK_DIR}/buildroot/output/rockchip_rk3588/host/bin/qmake
